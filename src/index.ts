165ad61963d775c28915689936877eb6376d6dfe
James <jetter019@msn.com>
2017-09-23 15:57:24 +0200

    Added a TODO.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as opn from 'opn';
 import { STEAM_URL } from './constants';
 import { Steam } from './steam';
 
+// TODO: The pre commit hook should only check the hygiene of the files that are being committed.
+
 export class SteamFactory {
 
     public createSteam(callback: (err?: Error, steam?: Steam) => void): void {
