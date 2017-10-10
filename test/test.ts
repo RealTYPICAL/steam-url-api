7264d4709afbc82efb505592f892ab1f2bba800e
James <jetter019@msn.com>
2017-10-10 23:44:48 +0200

    These tests are now way better. Test whole library.

--- test/test.ts
+++ test/test.ts
@@ -2,49 +2,56 @@
 // TODO: Will probably need a similar node module to be written to close windows for clean up.
 // https://msdn.microsoft.com/en-us/library/windows/desktop/ms632682(v=vs.85).aspx
 // TODO: I could probably make this module faster by writing it in c++ and calling it from node...
-const monitor = require('active-window'); // tslint:disable-line
 import { assert, expect } from 'chai';
 import { Library, types } from 'ffi';
+import { readCString } from 'ref';
 import { createSteam, ISteam } from '../src/index';
 
 const stringPtr = types.CString;
 
 const lib = new Library('user32', {
+    CloseWindow: [
+        'long', ['long'],
+    ],
     GetForegroundWindow: [
         'long', [],
     ],
     GetWindowTextA: [
         'long', ['long', stringPtr, 'int'],
     ],
+    GetWindowTextLengthA: [
+        'long', ['long'],
+    ],
 });
+
+function getForegroundWindowName(): string {
+    const hwnd = lib.GetForegroundWindow();
+    const buf = new Buffer(lib.GetWindowTextLengthA(hwnd) + 1);
+    lib.GetWindowTextA(hwnd, buf, buf.length);
+    return readCString(buf);
+}
 
 describe('Base Steam API Tests', () => {
 
-    before(() => {
-        // monitor.getActiveWindow(window => {
-            // Warm up active window API before using it for testing.
-            // Otherwise you get timeouts.
-        // });
-        const blah = lib.GetForegroundWindow();
-        const bigTest = 'jweofijwef';
-    });
-
-    it('has steam installed.', () => {
-        createSteam((err: Error, steam: ISteam) => {
-            assert(steam, 'Steam is initialised.');
-        });
-    });
+    // it('has steam installed.', () => {
+    //     createSteam((err: Error, steam: ISteam) => {
+    //         assert(steam, 'Steam is initialised.');
+    //     });
+    // });
 
     it('can add non steam game.', done => {
         createSteam((err: Error, steam: ISteam) => {
             steam.addNonSteamGame(steamErr => {
                 assert(!steamErr, 'There was an error adding non steam game.');
-                monitor.getActiveWindow(window => {
-                    assert(window.app === 'Steam', 'Application should be Steam.');
-                    assert(window.title === 'Add a Game', 'Should be trying to add a game.');
+                setTimeout(() => {
+                    assert.equal(getForegroundWindowName(), 'Add a Game', 'Add non steam game should be open.');
                     done();
-                });
+                }, 200);
             });
         });
+    });
+
+    afterEach(() => {
+        lib.CloseWindow(lib.GetForegroundWindow());
     });
 });
